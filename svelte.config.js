6e60e18777e694e1af2c81911b33c7283c7f46e7
iambasantarai <iambasantarai@gmail.com>
2024-09-01 16:53:14 +0545

    Implement syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,24 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
+import { escapeSvelte, mdsvex } from 'mdsvex';
+import { bundledLanguages, bundledThemes, createHighlighter } from 'shiki';
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md']
+	extensions: ['.md'],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await createHighlighter({
+				themes: Object.keys(bundledThemes),
+				langs: Object.keys(bundledLanguages)
+			});
+
+			const html = escapeSvelte(
+				highlighter.codeToHtml(code, { lang, theme: 'catppuccin-macchiato' })
+			);
+			return `{@html \`${html}\`}`;
+		}
+	}
 };
 
 /** @type {import('@sveltejs/kit').Config} */
