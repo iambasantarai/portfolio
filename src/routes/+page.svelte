6fcf5fe5a95afe5ee95602ac972d6adbc516f236
iambasantarai <iambasantarai@gmail.com>
2024-09-10 22:10:00 +0545

    Add education section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -78,6 +78,32 @@
 	</div>
 
 	<div class="mt-8">
+		<h2 class="text-2xl font-bold text-zinc-400">Education</h2>
+
+		<ol class="mt-6 space-y-4">
+			<li class="flex gap-4">
+				<dl class="flex flex-auto flex-wrap gap-x-2">
+					<dt class="sr-only">Course</dt>
+					<dd class="w-full flex-none text-base font-semibold text-zinc-100">
+						BSc.CSIT
+					</dd>
+					<dt class="sr-only">About</dt>
+					<dd class="text-xs text-zinc-400">[TU]: Prime College, Kathmandu</dd>
+					<dt class="sr-only">Date</dt>
+					<dd
+						class="ml-auto text-xs text-zinc-500"
+						aria-label="Aug, 2023 until Present"
+					>
+						<time datetime="2023">2019</time>
+						<span aria-hidden="true">—</span>
+						<time datetime="2024">Present</time>
+					</dd>
+				</dl>
+			</li>
+		</ol>
+	</div>
+
+	<div class="mt-8">
 		<h2 class="text-2xl font-bold text-zinc-400">Writings</h2>
 		<p class="text-xs leading-normal text-zinc-400">Basically experiments...</p>
 
