efa995a2596300d50498ad5562ce4cd7c1e0a77a
iambasantarai <iambasantarai@gmail.com>
2024-09-15 19:06:27 +0545

    WIP navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
 	import Footer from './footer.svelte';
 </script>
 
-<div class="min-h-screen bg-zinc-950 p-4 text-zinc-200">
-	<div class="mx-auto max-w-xl">
+<div class="min-h-screen bg-zinc-950 text-zinc-200 antialiased">
+	<div class="mx-auto max-w-xl p-4">
 		<Header />
 
 		<main>
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,5 +1,18 @@
-<script>
-	import * as config from '$lib/config';
-</script>
-
-<header></header>
+<header>
+	<div class="flex items-center justify-center">
+		<nav
+			class="flex gap-1 rounded-full border border-zinc-100/15 bg-zinc-200/10 p-1 backdrop-blur"
+		>
+			<a
+				href="/"
+				class="rounded-full px-2 py-1.5 transition duration-300 hover:bg-zinc-200/5"
+				>Home</a
+			>
+			<a
+				href="/writings"
+				class="rounded-full px-2 py-1.5 transition duration-300 hover:bg-zinc-200/5"
+				>Writings</a
+			>
+		</nav>
+	</div>
+</header>
