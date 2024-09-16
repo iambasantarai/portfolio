bc8a8f9ff4eccfa3c354809d5ebd97c7cd9475e9
iambasantarai <iambasantarai@gmail.com>
2024-09-16 22:49:32 +0545

    Update prose style

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -10,13 +10,22 @@
 	<meta property="og:title" content={data.metadata.title} />
 </svelte:head>
 
-<article>
-	<hgroup>
-		<h1>{data.metadata.title}</h1>
-		<p>{formatDate(data.metadata.date)}</p>
-	</hgroup>
+<article class="mt-8">
+	<header class="flex flex-col">
+		<h1 class="mt-6 text-4xl font-bold tracking-tight text-zinc-100">
+			{data.metadata.title}
+		</h1>
+		<time
+			datetime="2022-09-05"
+			class="order-first flex items-center text-base text-zinc-500"
+		>
+			<span class="h-4 w-0.5 rounded-full bg-zinc-500"></span><span class="ml-3"
+				>{formatDate(data.metadata.date)}</span
+			>
+		</time>
+	</header>
 
-	<div class="prose prose-zinc">
+	<div class="prose prose-invert mt-8">
 		<svelte:component this={data.content} />
 	</div>
 </article>
