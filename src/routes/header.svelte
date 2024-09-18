a0c85e019a866a81bc0898c027e0f36b53a9dccc
iambasantarai <iambasantarai@gmail.com>
2024-09-18 21:31:59 +0545

    Prefix with the base path

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,15 +1,19 @@
+<script lang="ts">
+    import {base} from "$app/paths";
+</script>
+
 <header>
 	<div class="mt-2 flex items-center justify-center">
 		<nav
 			class="flex gap-1 rounded-full border border-zinc-100/15 bg-zinc-200/10 p-1 backdrop-blur"
 		>
 			<a
-				href="/"
+				href="{base}/"
 				class="rounded-full px-2 py-1.5 transition duration-300 hover:bg-zinc-200/5"
 				>Home</a
 			>
 			<a
-				href="/writings"
+				href="{base}/writings"
 				class="rounded-full px-2 py-1.5 transition duration-300 hover:bg-zinc-200/5"
 				>Writings</a
 			>
