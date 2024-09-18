c4f2cd5f1c15f9e724291c2dc53e6f7b3d07bb49
iambasantarai <iambasantarai@gmail.com>
2024-09-18 22:11:23 +0545

    Add resume file path in download link

--- src/routes/experience.svelte
+++ src/routes/experience.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    let resumeURL = '/basanta_rai_resume_2024.pdf';
+</script>
+
 <div class="mt-8">
 	<h2 class="text-2xl font-bold text-zinc-400">Experience</h2>
 
@@ -43,8 +47,9 @@
 	</ol>
 
 	<a
+        href={resumeURL}
+        download="basanta-rai-resume.pdf"
 		class="group mt-6 inline-flex w-full items-center justify-center gap-2 rounded-lg border border-zinc-200/15 bg-zinc-800/50 px-3 py-2 text-sm font-medium text-zinc-300 outline-offset-2 transition hover:bg-zinc-800 hover:text-zinc-50 active:bg-zinc-800/50 active:text-zinc-50/70 active:transition-none"
-		href="/"
 	>
 		Download Resume
 		<svg
