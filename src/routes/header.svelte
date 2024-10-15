0b050b18a5d7a52a1dd653ebddee86e8ad3f42b4
iambasantarai <iambasantarai@gmail.com>
2024-10-15 19:15:59 +0545

    Add current page indicator

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import {page} from "$app/stores";
+</script>
+
 <header>
 	<div class="mt-2 flex items-center justify-center">
 		<nav
@@ -5,12 +9,12 @@
 		>
 			<a
 				href="/"
-				class="rounded-full px-2 py-1.5 transition duration-300 hover:bg-zinc-200/5"
+				class={`rounded-full px-2 py-1.5 transition ease-in-out duration-300 hover:bg-zinc-200/5 ${$page.url.pathname === '/' ? 'underline underline-offset-4' : ''}`}
 				>Home</a
 			>
 			<a
 				href="/writings"
-				class="rounded-full px-2 py-1.5 transition duration-300 hover:bg-zinc-200/5"
+				class={`rounded-full px-2 py-1.5 transition ease-in-out duration-300 hover:bg-zinc-200/5 ${$page.url.pathname === '/writings' ? 'underline underline-offset-4' : ''}`}
 				>Writings</a
 			>
 		</nav>
