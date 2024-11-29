1ba333414cf39830fec4f670dc92bf9c2aa6d0f0
iambasantarai <iambasantarai@gmail.com>
2024-11-29 22:05:31 +0545

    Clean up uses section

--- src/routes/tools.svelte
+++ src/routes/tools.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
 	import DockerIcon from '../assets/icons/docker.svelte';
-	import ExpressIcon from '../assets/icons/express.svelte';
 	import GitIcon from '../assets/icons/git.svelte';
-	import GoIcon from '../assets/icons/golang.svelte';
-	import JsIcon from '../assets/icons/js.svelte';
-	import LaravelIcon from '../assets/icons/laravel.svelte';
 	import LinuxIcon from '../assets/icons/linux.svelte';
-	import LuaIcon from '../assets/icons/lua.svelte';
 	import NeovimIcon from '../assets/icons/neovim.svelte';
-	import NodeJsIcon from '../assets/icons/nodejs.svelte';
-	import PHPIcon from '../assets/icons/php.svelte';
 	import TmuxIcon from '../assets/icons/tmux.svelte';
-	import TsIcon from '../assets/icons/ts.svelte';
 
-	const toolBoxItems = [
+	const tools = [
 		{
 			title: 'Linux',
 			icon: LinuxIcon
@@ -34,45 +26,13 @@
 			title: 'Git',
 			icon: GitIcon
 		},
-		{
-			title: 'Lua',
-			icon: LuaIcon
-		},
-		{
-			title: 'Go',
-			icon: GoIcon
-		},
-		{
-			title: 'Javascript',
-			icon: JsIcon
-		},
-		{
-			title: 'Typescript',
-			icon: TsIcon
-		},
-		{
-			title: 'Node.js',
-			icon: NodeJsIcon
-		},
-		{
-			title: 'Express',
-			icon: ExpressIcon
-		},
-		{
-			title: 'PHP',
-			icon: PHPIcon
-		},
-		{
-			title: 'Laravel',
-			icon: LaravelIcon
-		}
 	];
 </script>
 
 <div class="mt-12">
-	<h2 class="text-2xl font-bold text-zinc-400">Toolbox</h2>
+	<h2 class="text-2xl font-bold text-zinc-400">Uses</h2>
 	<p class="text-xs leading-normal text-zinc-400">
-		Tools and technologies that i'm familiar with...
+		Development tools
 	</p>
 
 	<div
@@ -81,14 +41,14 @@
 		<div
 			class="flex flex-none animate-move-left gap-6 py-0.5 [animation-duration:45s]"
 		>
-			{#each [...toolBoxItems, ...toolBoxItems] as item}
+			{#each [...tools, ...tools] as tool}
 				<div
 					class="inline-flex items-center gap-4 rounded-lg px-3 py-2 outline outline-2 outline-zinc-200/10"
 				>
 					<span>
-						<svelte:component this={item.icon} />
+						<svelte:component this={tool.icon} />
 					</span>
-					<span class="font-semibold">{item.title}</span>
+					<span class="font-semibold">{tool.title}</span>
 				</div>
 			{/each}
 		</div>
@@ -100,14 +60,14 @@
 		<div
 			class="flex flex-none animate-move-right gap-6 py-0.5 pr-6 [animation-duration:30s]"
 		>
-			{#each [...toolBoxItems, ...toolBoxItems] as item}
+			{#each [...tools, ...tools] as tool}
 				<div
 					class="inline-flex items-center gap-4 rounded-lg px-3 py-2 outline outline-2 outline-zinc-200/10"
 				>
 					<span>
-						<svelte:component this={item.icon} />
+						<svelte:component this={tool.icon} />
 					</span>
-					<span class="font-semibold">{item.title}</span>
+					<span class="font-semibold">{tool.title}</span>
 				</div>
 			{/each}
 		</div>
