083e4f30b7615b2a62c3936b0627100be98a8f1b
iambasantarai <iambasantarai@gmail.com>
2024-12-16 22:58:25 +0545

    Update tools section heading

--- src/lib/components/Tools.svelte
+++ src/lib/components/Tools.svelte
@@ -30,7 +30,7 @@
 </script>
 
 <div class="mt-12">
-	<h2 class="text-2xl font-bold text-zinc-200">Uses</h2>
+	<h2 class="text-2xl font-bold text-zinc-200">Tools</h2>
 	<p class="mt-2 text-sm leading-normal text-zinc-400">
 		The tools I rely on for my Personal Development Environment(PDE)
 	</p>
