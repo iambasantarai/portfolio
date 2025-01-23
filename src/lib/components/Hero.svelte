83076257c2d6666e224782c933180c4f4ee68725
iambasantarai <iambasantarai@gmail.com>
2025-01-23 14:41:51 +0545

    Wrap image inside dashed frame

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,19 +1,218 @@
 <div class="mt-8">
 	<div class="flex flex-col items-center">
-		<div class="h-24 w-24 overflow-hidden rounded-full bg-zinc-800">
-			<a
-				aria-label="Home"
-				class="pointer-events-auto block h-24 w-24 origin-left"
-				href="/"
+		<div class="relative my-4 flex">
+			<img
+				src="/basanta.png"
+				alt="Basanta Rai"
+				class="h-24 w-24 object-cover"
+				width="auto"
+				height="auto"
+			/>
+
+			<!-- dashed frame -->
+			<div
+				class="absolute left-0 top-1/2 z-0 h-[175px] w-[1px] -translate-y-1/2"
 			>
-				<img
-					src="/basanta.png"
-					alt="Basanta Rai"
-					class="h-full w-full object-cover transition duration-300 hover:scale-125"
-					width="auto"
-					height="auto"
-				/>
-			</a>
+				<svg class="absolute inset-0 text-gray-300" height="100%" width="100%">
+					<defs>
+						<pattern
+							height="6"
+							id="fading-dashed-line-left-pattern"
+							patternUnits="userSpaceOnUse"
+							width="6"
+							x="0"
+							y="0"
+							><line
+								stroke="currentColor"
+								stroke-width="1"
+								x1="0"
+								x2="0"
+								y1="0"
+								y2="3"
+							></line></pattern
+						>
+						<linearGradient
+							gradientUnits="objectBoundingBox"
+							id="fading-dashed-line-left-gradient"
+							x1="0.5"
+							x2="0.5"
+							y1="0"
+							y2="1"
+						>
+							<stop offset="0%" stop-color="white" stop-opacity="0"></stop>
+							<stop offset="25%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="75%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="100%" stop-color="white" stop-opacity="0"></stop>
+						</linearGradient>
+						<mask id="fading-dashed-line-left-mask"
+							><rect
+								fill="url(#fading-dashed-line-left-gradient)"
+								height="100%"
+								width="100%"
+							></rect></mask
+						>
+					</defs>
+					<rect
+						fill="url(#fading-dashed-line-left-pattern)"
+						height="100%"
+						mask="url(#fading-dashed-line-left-mask)"
+						width="100%"
+					></rect>
+				</svg>
+			</div>
+
+			<div
+				class="absolute right-0 top-1/2 z-0 h-[175px] w-[1px] -translate-y-1/2"
+			>
+				<svg class="absolute inset-0 text-gray-300" height="100%" width="100%">
+					<defs>
+						<pattern
+							height="6"
+							id="fading-dashed-line-right-pattern"
+							patternUnits="userSpaceOnUse"
+							width="6"
+							x="0"
+							y="0"
+							><line
+								stroke="currentColor"
+								stroke-width="1"
+								x1="0"
+								x2="0"
+								y1="0"
+								y2="3"
+							></line></pattern
+						>
+						<linearGradient
+							gradientUnits="objectBoundingBox"
+							id="fading-dashed-line-right-gradient"
+							x1="0.5"
+							x2="0.5"
+							y1="0"
+							y2="1"
+						>
+							<stop offset="0%" stop-color="white" stop-opacity="0"></stop>
+							<stop offset="25%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="75%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="100%" stop-color="white" stop-opacity="0"></stop>
+						</linearGradient>
+						<mask id="fading-dashed-line-right-mask"
+							><rect
+								fill="url(#fading-dashed-line-right-gradient)"
+								height="100%"
+								width="100%"
+							></rect></mask
+						>
+					</defs>
+					<rect
+						fill="url(#fading-dashed-line-right-pattern)"
+						height="100%"
+						mask="url(#fading-dashed-line-right-mask)"
+						width="100%"
+					></rect>
+				</svg>
+			</div>
+
+			<div
+				class="absolute left-1/2 top-0 z-0 h-[1px] w-[195px] -translate-x-1/2"
+			>
+				<svg class="absolute inset-0 text-gray-300" height="100%" width="100%">
+					<defs>
+						<pattern
+							height="6"
+							id="fading-dashed-line-top-pattern"
+							patternUnits="userSpaceOnUse"
+							width="6"
+							x="0"
+							y="0"
+							><line
+								stroke="currentColor"
+								stroke-width="1"
+								x1="0"
+								x2="3"
+								y1="0"
+								y2="0"
+							></line></pattern
+						>
+						<linearGradient
+							gradientUnits="objectBoundingBox"
+							id="fading-dashed-line-top-gradient"
+							x1="0"
+							x2="1"
+							y1="0.5"
+							y2="0.5"
+						>
+							<stop offset="0%" stop-color="white" stop-opacity="0"></stop>
+							<stop offset="25%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="75%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="100%" stop-color="white" stop-opacity="0"></stop>
+						</linearGradient>
+						<mask id="fading-dashed-line-top-mask"
+							><rect
+								fill="url(#fading-dashed-line-top-gradient)"
+								height="100%"
+								width="100%"
+							></rect></mask
+						>
+					</defs>
+					<rect
+						fill="url(#fading-dashed-line-top-pattern)"
+						height="100%"
+						mask="url(#fading-dashed-line-top-mask)"
+						width="100%"
+					></rect>
+				</svg>
+			</div>
+
+			<div
+				class="absolute bottom-0 left-1/2 h-[1px] w-[195px] -translate-x-1/2"
+			>
+				<svg class="absolute inset-0 text-gray-300" height="100%" width="100%">
+					<defs>
+						<pattern
+							height="6"
+							id="fading-dashed-line-bottom-pattern"
+							patternUnits="userSpaceOnUse"
+							width="6"
+							x="0"
+							y="0"
+							><line
+								stroke="currentColor"
+								stroke-width="1"
+								x1="0"
+								x2="3"
+								y1="0"
+								y2="0"
+							></line></pattern
+						>
+						<linearGradient
+							gradientUnits="objectBoundingBox"
+							id="fading-dashed-line-bottom-gradient"
+							x1="0"
+							x2="1"
+							y1="0.5"
+							y2="0.5"
+						>
+							<stop offset="0%" stop-color="white" stop-opacity="0"></stop>
+							<stop offset="25%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="75%" stop-color="white" stop-opacity="1"></stop>
+							<stop offset="100%" stop-color="white" stop-opacity="0"></stop>
+						</linearGradient>
+						<mask id="fading-dashed-line-bottom-mask"
+							><rect
+								fill="url(#fading-dashed-line-bottom-gradient)"
+								height="100%"
+								width="100%"
+							></rect></mask
+						>
+					</defs>
+					<rect
+						fill="url(#fading-dashed-line-bottom-pattern)"
+						height="100%"
+						mask="url(#fading-dashed-line-bottom-mask)"
+						width="100%"
+					></rect>
+				</svg>
+			</div>
 		</div>
 
 		<div class="mt-4">
