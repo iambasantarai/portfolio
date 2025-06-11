cb60330843edcd04da27ff4e12fdc56c985889c8
iambasantarai <developer.basanta@gmail.com>
2025-06-11 22:22:54 +0545

    Make copyright section center aligned

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="mb-4 mt-8">
-	<p class="text-sm text-zinc-400">
+	<p class="text-sm text-zinc-400 text-center">
 		{new Date().getFullYear()} &copy; {config.name}.
 	</p>
 </footer>
