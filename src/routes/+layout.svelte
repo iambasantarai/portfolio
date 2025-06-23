010723afd305852db0c253b3c88c9b1fa2b6d91a
iambasantarai <developer.basanta@gmail.com>
2025-06-23 11:29:21 +0545

    Add meta tags for better SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 	export let data: { url: string };
 </script>
 
+<svelte:head>
+  <meta property="description" content="Personal portfolio" />
+  <meta property="keywords" content="Basanta Rai, Basanta Rana Sampang Rai, Software Developer, Backend Developer, Vimmer" />
+  <meta property="author" content="Basanta Rai" />
+</svelte:head>
+
 <div class="min-h-screen bg-zinc-950 text-zinc-200 antialiased">
 	<div class="mx-auto max-w-xl p-4">
 		<Header />
